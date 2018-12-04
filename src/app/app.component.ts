0d7a340cd5175c680d72748d6eaaea7ed50bd8cc
DAcajabon <diego.acajabon@gmail.com>
2018-12-04 15:46:25 -0600

    Hacer por id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
+
   title = 'Ajedrez';
+
+  //  https://codepen.io/Darkstron/post/tablero-de-ajedrez-usando-tablas-y-css
+
 }
